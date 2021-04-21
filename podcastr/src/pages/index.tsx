d9e219bf46182e7dede4de8e45fde962390ac832
Alexandre Santos <sasle57@gmail.com>
2021-04-21 15:14:47 -0300

    improving api and date display

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
   id: string;
   title: string;
   members: string;
+  publishedAt: string;
 }
 
 type HomeProps = {
@@ -13,22 +17,39 @@ type HomeProps = {
 
 export default function Home(props: HomeProps) {
   return (
-    <h1>Index</h1>
+    <>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
   )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await api.get('episodes', {
     params: {
-      _limit:12,
+      _limit: 12,
       _sort: 'published_at',
       _order: 'desc'
     }
   });
 
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url
+    }
+  })
+
   return {
     props: {
-      episodes: data
+      episodes
     },
     revalidate: 60 * 60 * 8
   }
