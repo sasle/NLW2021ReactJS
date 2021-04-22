2960d1704e5770f6cb85c2cbb80eb48fcf5a4921
Alexandre Santos <sasle57@gmail.com>
2021-04-22 11:18:50 -0300

    creating context api

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -6,6 +6,8 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 import styles from './home.module.scss';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 type Episode = {
   id: string;
@@ -24,6 +26,9 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+
+  const {play} = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -43,8 +48,8 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                     <span>{episode.publishedAt}</span>
                     <span>{episode.durationAsString}</span>
                   </div>
-                  <button type="button">
-                    <img src="play-green.svg" alt="Tocar episódio" />
+                  <button type="button" onClick={() => play(episode)}>
+                    <img src="/play-green.svg" alt="Tocar episódio"/>
                   </button>
                 </li>
               )
