d45889a079849347476ad699781ca20d62e58046
Alexandre Santos <sasle57@gmail.com>
2021-04-22 11:29:52 -0300

    slider and styling

--- podcastr/src/components/Player/index.tsx
+++ podcastr/src/components/Player/index.tsx
@@ -1,9 +1,14 @@
+import Image from 'next/image';
 import { useContext } from 'react';
+import Slider from 'rc-slider';
+
+import 'rc-slider/assets/index.css';
+
 import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
 
 export default function Player() {
-    const {episodeList, currentEpisodeIndex} = useContext(PlayerContext);
+    const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
 
     const episode = episodeList[currentEpisodeIndex];
 
@@ -11,18 +16,41 @@ export default function Player() {
         <div className={styles.playerContainer}>
             <header>
                 <img src="playing.svg" alt="Tocando agora" />
-                <strong>Tocando agora {episode?.title}</strong>
+                <strong>Tocando agora</strong>
             </header>
-
-            <div className={styles.emptyPlayer}>
-                <strong>Selecione um podcast para ouvir</strong>
-            </div>
-
-            <footer className={styles.empty}>
+            {
+                episode ?
+                    (
+                        <div className={styles.currentEpisode}>
+                            <Image width={592} height={592} src={episode.thumbnail} objectFit="cover" />
+                            <strong>{episode.title}</strong>
+                            <span>{episode.members}</span>
+                        </div>
+                    )
+                    :
+                    (
+                        <div className={styles.emptyPlayer}>
+                            <strong>Selecione um podcast para ouvir</strong>
+                        </div>
+                    )
+            }
+            <footer className={!episode ? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
-                        <div className={styles.emptySlider} />
+                        {
+                            episode ?
+                                (
+                                    <Slider trackStyle={{backgroundColor: '#04d361'}}
+                                    railStyle={{backgroundColor: '#9f75ff'}}
+                                    handleStyle={{backgroundColor: '#04d361', borderWidth: 4}}
+                                    />
+                                )
+                                :
+                                (
+                                    <div className={styles.emptySlider} />
+                                )
+                        }
                     </div>
                     <span>00:00</span>
                 </div>
@@ -45,6 +73,6 @@ export default function Player() {
                     </button>
                 </div>
             </footer>
-        </div>
+        </div >
     );
 }
